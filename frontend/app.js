a8a5ac11a8fe0e3d7b8851a419fd430515d0aaec
Abraham <rabrah25@MGC000002565.paloalto.ford.com>
2017-11-20 16:26:41 -0800

    oath with satellite, implicit

--- frontend/app.js
+++ frontend/app.js
@@ -1,11 +1,11 @@
 var angular = require('angular');
 var bootstrap = require('bootstrap/dist/css/bootstrap.css');
-angular.module('meanApp',[require('angular-route')]);
+angular.module('meanApp',[require('angular-route'), require('satellizer')]);
 
 require('./controllers');
 require('./services'); 
 
-function config ($routeProvider, $locationProvider) {
+function config ($routeProvider, $locationProvider, $authProvider) {
     $routeProvider
       .when('/', {
         template: require('./login.html'),
@@ -33,7 +33,25 @@ function config ($routeProvider, $locationProvider) {
 
     // use the HTML5 History API
     $locationProvider.html5Mode(true);
+  
+  
+  $authProvider.google({
+  url: '/auth/google',
+  clientId: '134937980315-ev8q4v5t1im2uvetsjpa5486ims4a572.apps.googleusercontent.com',
+  responseType: 'token',
+  authorizationEndpoint: 'https://accounts.google.com/o/oauth2/auth',
+  redirectUri: window.location.origin,
+  requiredUrlParams: ['scope'],
+  optionalUrlParams: ['display'],
+  scope: ['profile', 'email'],
+  scopePrefix: 'openid',
+  scopeDelimiter: ' ',
+  display: 'popup',
+  oauthType: '2.0',
+  popupOptions: { width: 452, height: 633 }
+});
   }
+
 
 function run($rootScope, $location, authentication) {
     $rootScope.$on('$routeChangeStart', function(event, nextRoute, currentRoute) {
@@ -45,7 +63,7 @@ function run($rootScope, $location, authentication) {
   
  angular
     .module('meanApp')
-    .config(['$routeProvider', '$locationProvider', config])
+    .config(['$routeProvider', '$locationProvider', '$authProvider', config])
     .run(['$rootScope', '$location', 'authentication', run]);
 
 
--- frontend/controllers/login.controller.js
+++ frontend/controllers/login.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
-LoginCtrl.$inject = ['$location', 'authentication'];
+LoginCtrl.$inject = ['$location', 'authentication', '$auth'];
 
-function LoginCtrl($location, authentication) {
+function LoginCtrl($location, authentication, $auth) {
     var vm = this;
 
     vm.credentials = { email : "", password : "" };
@@ -15,6 +15,19 @@ function LoginCtrl($location, authentication) {
         });
     };
 
+    vm.authenticate = function(provider) {
+      console.log("logging with ",provider);
+      $auth.authenticate('google')
+      .then(function(response) {
+        console.log(response);
+        return $http.get('https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token='+response.access_token).then(function success(res){
+          console.log(res);        
+      }, function error(err){/*Stub*/});
+      })
+      .catch(function(response) {
+      // Something went wrong.
+      });
+    };
 }
 
 module.exports = LoginCtrl;
